cf408609d0fdd583c61cc95b7499976a0cb60959
youkorr <96953496+youkorr@users.noreply.github.com>
2025-04-30 22:27:37 +0200

    Update __init__.py

--- components/movie/__init__.py
+++ components/movie/__init__.py
@@ -1,100 +1,34 @@
-"""
-Composant ESPHome pour la lecture de vidéos MP4 depuis des adresses IP partagées.
-Compatible avec ESP32-S3 et ESP-IDF 5.1.5.
-"""
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import display
 from esphome.const import CONF_ID
-from esphome.components import button
-from esphome.components.button import Button
 
-DEPENDENCIES = ["esp32"]
-AUTO_LOAD = ["network"]
+DEPENDENCIES = ['display']
+MULTI_CONF = True
 
-# Configuration constants
-CONF_DISPLAY_WIDTH = "display_width"
-CONF_DISPLAY_HEIGHT = "display_height" 
-CONF_URL = "url"
-CONF_PLAY_BUTTON = "play_button"
-CONF_STOP_BUTTON = "stop_button"
-CONF_PAUSE_BUTTON = "pause_button"
+movie_ns = cg.esphome_ns.namespace('movie')
+MoviePlayer = movie_ns.class_('MoviePlayer', cg.Component)
 
-# Namespace
-movie_ns = cg.esphome_ns.namespace("movie")
-MoviePlayer = movie_ns.class_("MoviePlayer", cg.Component)
-
-# Actions
-PlayAction = movie_ns.class_("PlayAction", cg.Action)
-StopAction = movie_ns.class_("StopAction", cg.Action)
-PauseAction = movie_ns.class_("PauseAction", cg.Action)
-ResumeAction = movie_ns.class_("ResumeAction", cg.Action)
-SetUrlAction = movie_ns.class_("SetUrlAction", cg.Action)
-
-# Config Schema
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MoviePlayer),
-    cv.Optional(CONF_DISPLAY_WIDTH, default=320): cv.positive_int,
-    cv.Optional(CONF_DISPLAY_HEIGHT, default=240): cv.positive_int,
-    cv.Optional(CONF_URL): cv.string,
+    cv.Optional('display_width', default=320): cv.int_,
+    cv.Optional('display_height', default=240): cv.int_,
+    cv.Optional('buffer_size', default=8192): cv.int_,
+    cv.Optional('fps', default=15): cv.int_,
+    cv.Optional('http_timeout', default=5000): cv.int_,
+    cv.Optional('http_buffer_size', default=4096): cv.int_,
 }).extend(cv.COMPONENT_SCHEMA)
 
-# Validation pour les actions
-MOVIE_PLAY_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(MoviePlayer),
-    cv.Required(CONF_URL): cv.string,
-})
-MOVIE_STOP_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(MoviePlayer),
-})
-MOVIE_PAUSE_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(MoviePlayer),
-})
-MOVIE_RESUME_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(MoviePlayer),
-})
-MOVIE_SET_URL_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.use_id(MoviePlayer),
-    cv.Required(CONF_URL): cv.string,
-})
-
-# Enregistrer les actions
-@cg.action_registry.register("movie.play", PlayAction, MOVIE_PLAY_SCHEMA)
-def movie_play_to_code(config, action_id, template_arg, args):
-    paren = yield cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    yield cg.add(var.set_url(config[CONF_URL]))
-    yield var
-
-@cg.action_registry.register("movie.stop", StopAction, MOVIE_STOP_SCHEMA)
-def movie_stop_to_code(config, action_id, template_arg, args):
-    paren = yield cg.get_variable(config[CONF_ID])
-    return cg.new_Pvariable(action_id, template_arg, paren)
-
-@cg.action_registry.register("movie.pause", PauseAction, MOVIE_PAUSE_SCHEMA)
-def movie_pause_to_code(config, action_id, template_arg, args):
-    paren = yield cg.get_variable(config[CONF_ID])
-    return cg.new_Pvariable(action_id, template_arg, paren)
-
-@cg.action_registry.register("movie.resume", ResumeAction, MOVIE_RESUME_SCHEMA)
-def movie_resume_to_code(config, action_id, template_arg, args):
-    paren = yield cg.get_variable(config[CONF_ID])
-    return cg.new_Pvariable(action_id, template_arg, paren)
-
-@cg.action_registry.register("movie.set_url", SetUrlAction, MOVIE_SET_URL_SCHEMA)
-def movie_set_url_to_code(config, action_id, template_arg, args):
-    paren = yield cg.get_variable(config[CONF_ID])
-    var = cg.new_Pvariable(action_id, template_arg, paren)
-    yield cg.add(var.set_url(config[CONF_URL]))
-    yield var
-
-def to_code(config):
+async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
+    await cg.register_component(var, config)
     
-    if CONF_URL in config:
-        cg.add(var.set_url(config[CONF_URL]))
-    
-    cg.add(var.set_dimensions(config[CONF_DISPLAY_WIDTH], config[CONF_DISPLAY_HEIGHT]))
+    cg.add(var.set_display_width(config['display_width']))
+    cg.add(var.set_display_height(config['display_height']))
+    cg.add(var.set_buffer_size(config['buffer_size']))
+    cg.add(var.set_fps(config['fps']))
+    cg.add(var.set_http_timeout(config['http_timeout']))
+    cg.add(var.set_http_buffer_size(config['http_buffer_size']))
+
 
 

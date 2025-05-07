d4239f7d668fa6f1a7b59df5ffdf3777324874e3
youkorr <96953496+youkorr@users.noreply.github.com>
2025-05-07 23:46:44 +0200

    Update video_camera.h

--- components/video_camera/video_camera.h
+++ components/video_camera/video_camera.h
@@ -2,59 +2,53 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/hal.h"
-#include "esphome/core/log.h"
+#include "esphome/components/image/image.h"
+
+// Ajout de l'inclusion nécessaire pour TaskHandle_t
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
 namespace esphome {
 namespace video_camera {
 
-// Structure pour stocker les informations d'une frame
-struct RtspFrame {
-  uint8_t *buffer;
-  size_t size;
-  int width;
-  int height;
-  bool is_jpeg;
+struct CameraFrame {
+  uint8_t *buffer{nullptr};
+  size_t size{0};
+  bool is_jpeg{false};
+  uint32_t width{0};
+  uint32_t height{0};
 };
 
-// Callback lorsqu'une nouvelle frame est disponible
-using FrameCallback = std::function<void(const RtspFrame &frame)>;
+using camera_frame_callback_t = std::function<void(const CameraFrame &)>;
 
 class VideoCamera : public Component {
  public:
-  void set_url(const std::string &url) { url_ = url; }
-  void set_fps(int fps) { fps_ = fps; }
-  
   void setup() override;
   void loop() override;
   void dump_config() override;
   
-  // Méthode pour enregistrer un callback lorsqu'une nouvelle frame est disponible
-  void add_frame_callback(FrameCallback &&callback) { frame_callbacks_.push_back(std::move(callback)); }
-  
-  // Obtenir l'URL du flux
+  void set_url(const std::string &url) { url_ = url; }
   const std::string &get_url() const { return url_; }
   
-  // Obtenir la dernière frame
-  const RtspFrame *get_last_frame() const { return &last_frame_; }
+  void set_fps(int fps) { fps_ = fps; }
+  int get_fps() const { return fps_; }
+  
+  void add_frame_callback(camera_frame_callback_t callback) {
+    frame_callbacks_.push_back(std::move(callback));
+  }
   
  protected:
-  std::string url_;
-  int fps_{1};
-  unsigned long last_frame_time_{0};
-  RtspFrame last_frame_{nullptr, 0, 0, 0, false};
-  
-  std::vector<FrameCallback> frame_callbacks_{};
-  
-  bool initialize_rtsp_client_();
-  bool fetch_rtsp_frame_();
+  static void rtsp_task(void *parameter);
   void call_frame_callbacks_();
   
-  // Task handle pour le thread de traitement RTSP
-  TaskHandle_t rtsp_task_handle_{nullptr};
-  bool rtsp_running_{false};
+  std::string url_;
+  int fps_{5};  // Fréquence d'images par défaut
   
-  // Méthode statique pour le thread de traitement RTSP
-  static void rtsp_task(void *parameter);
+  std::vector<camera_frame_callback_t> frame_callbacks_;
+  CameraFrame last_frame_;
+  
+  bool rtsp_running_{false};
+  TaskHandle_t rtsp_task_handle_{nullptr};  // Maintenant correctement déclaré avec l'inclusion de task.h
 };
 
 }  // namespace video_camera
